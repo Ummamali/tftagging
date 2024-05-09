8242883b7cfacdb6e95c11c2fb68767bf77d4eed
Ummam Ali (Lappy) <sblue7446@gmail.com>
2024-05-10 00:00:35 +0500

    added content

--- app/image/utils/utils.py
+++ app/image/utils/utils.py
@@ -6,23 +6,11 @@ import os
 
 def move_media_from_temp(media_items, user_id, dest_bucket_name, tags_to_insert={}):
     # first we move them in the database
-    with DBConnection() as db:
-        col_ocean = db['ocean']
-        new_item_for_bucket = []
-        for item_title in media_items:
-            media_object = {'path': '/', 'title': item_title,
-                            'tags': tags_to_insert.get(item_title, {'object': [], 'person': []})}
-            new_item_for_bucket.append(media_object)
 
-        db.ocean.update_one(
-            {"_id": ObjectId(user_id), "buckets.name": dest_bucket_name},
-            {"$push": {"buckets.$.items": {'$each': new_item_for_bucket}}}
-        )
-
-        # then we move them in real storage
-        source_folder_path = os.path.join(
-            os.getcwd(), 'content', user_id, '_temp')
-        dest_folder_path = os.path.join(
-            os.getcwd(), 'content', user_id, dest_bucket_name)
-        move_files(source_folder_path, dest_folder_path, media_items)
+    # then we move them in real storage
+    source_folder_path = os.path.join(
+        os.getcwd(), 'content', user_id, '_temp')
+    dest_folder_path = os.path.join(
+        os.getcwd(), 'content', user_id, dest_bucket_name)
+    move_files(source_folder_path, dest_folder_path, media_items)
     return True
